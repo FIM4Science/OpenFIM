9f95478b4b206a7c5df0f672ff3097ec5c4cbff9
Antonia <tonikoe98@gmail.com>
2024-05-08 11:35:29 +0200

    reorganize dataset

--- src/fim/data/datasets.py
+++ src/fim/data/datasets.py
@@ -4,6 +4,7 @@ from typing import Optional, Union
 import random
 import math
 from itertools import pairwise
+from datetime import timedelta
 
 import torch
 from datasets import DatasetDict, DownloadMode, get_dataset_split_names, load_dataset, Dataset
@@ -69,7 +70,22 @@ class BaseDataset(torch.utils.data.Dataset):
 
 class PatchedDataset(BaseDataset):
     """
-    Split each time series into context windows, y sequence of patches. Store corresponding target (horizon) values.
+    Split each time series into (overlapping) context windows, then into patches. Store corresponding target (horizon) values.
+
+    Steps:
+    1. split time series into context + horizon windows
+    2. each context window: split into patches and corresponding target values
+    3. select training/testing input: per context window: select patch 1 & 1. prediction; 1-2 & 2. prediction; 1-3 & 3. prediction,...
+    4. create masks
+        - point level: first patch: mask first r (random) points; last patch: mask points to fill up to patch_len_in; remaining patches: not masked.
+            True indicates that the point is masked out. Bases on masking strategy of Das et al. in Decoder-only paper.
+        - token level: indicates if corresponding patch is fully masked out
+    5. resulting features of a data entry:
+        - input: sequence of patches, padded with 0 if necessary, [max_nr_patches_per_context_window, patch_len_in]
+        - output: target values subsequent to the last non-padded input value, [patch_len_out]
+        - mask_point_level: mask on point level, [max_nr_patches_per_context_window, patch_len_in]
+        - mask_token_level: mask on token level, [max_nr_patches_per_context_window]
+        - start: start time of the considered patch sequence (note: currently dummy time, needs to be fixed it ever necessary)
     """
 
     def __init__(
@@ -91,53 +107,28 @@ class PatchedDataset(BaseDataset):
         self.patch_len_out = patch_len_out
         self.patch_len_in = patch_len_in
         self.overlap_context_windows = overlap_context_windows
-        self.skipped_time_series_counter = 0
+
+        # TODO: fix time if ever needed. Currently: dummy time
+        self.time_units_per_step = timedelta(hours=1)
+
+        self.max_nr_patches_per_context_window = math.ceil(self.max_context_len / self.patch_len_in)
 
         processed_data = []
 
         for row in self.data:
-            processed_data.extend(self._split_time_series(time_series=row["target"], time_start=row["start"]))
+            processed_data.extend(self._process_single_time_series(time_series=row["target"], time_start=row["start"]))
 
-        if self.skipped_time_series_counter > 0:
-            self.logger.warning(
-                f"Skipped {self.skipped_time_series_counter} of {len(self.data)} time series due to insufficient length for context and horizon windows."
-            )
         self.data = Dataset.from_list(processed_data)
         self.logger.debug("Dataset successfully divided into context windows and patches.")
 
-    def _split_time_series(self, time_series, time_start):
-        """
-        Split a time series into (overlapping) context windows, then into patches. Also pad patches and create masks.
-
-        Args:
-            time_series (List[float]): The time series to split.
-            time_start (int): The start time of the time series.
-
-        Returns:
-            List[dict]]: The data entries for the given time series. One entry consists of:
-                'input': the input sequence of patches, padded as necessary. [max_nr_patches_per_context_window, patch_len_in]
-                'output': the output sequence of target values, subsequent to the last non-padded input token/time series point. [patch_len_out]
-                'mask_point_level': the mask on point level. [max_nr_patches_per_context_window, patch_len_in]
-                    Following the masking strategy of Das et al, the first r (random number) points of the fist patch are
-                    masked out. Further, if the last patch is not full length, the remaining points are masked out.
-                    True indicates that the point is masked out.
-                'mask_token_level': the mask on token level. [max_nr_patches_per_context_window]
-                    Mask for each patch, indicating whether the patch is fully masked out.
-                    True indicates that the patch is fully masked out.
-                'start': the start time of the time series. Note: needs to be updated if time feature is ever relevant.
-        """
-        # Safety check to ensure context length can accommodate at least one input patch
-        if self.max_context_len < self.patch_len_in:
-            raise ValueError("max context length < input patch length.")
-        if len(time_series) <= self.max_context_len + self.patch_len_out:
-            self.skipped_time_series_counter += 1
+    def _process_single_time_series(self, time_series, time_start) -> list[dict]:
+        """Split time series into context windows and trigger patching function."""
+        if len(time_series) <= self.patch_len_out + 1:
             return []
 
-        # Calculate the maximum number of patches per context window
-        max_nr_patches_per_context_window = math.ceil(self.max_context_len / self.patch_len_in)
         processed_data = []
 
-        # Determine the starting indices for each context window in the series
+        # get start indices for each context window
         context_start_indices = list(
             range(
                 0,
@@ -147,64 +138,96 @@ class PatchedDataset(BaseDataset):
         )
 
         for context_start_id in context_start_indices:
-            # Calculate the start indices for each patch within the current context
-            patch_start_indices = [
-                context_start_id + patch_nr * self.patch_len_in
-                for patch_nr in range(0, max_nr_patches_per_context_window)
-            ]
-            # append index of last element of last patch
-            patch_start_indices.append(context_start_id + self.max_context_len)
-
-            # extract patches + subsequent predictions
-            patches = [time_series[start:end] for start, end in pairwise(patch_start_indices)]
-            predictions = [
-                time_series[patch_end : patch_end + self.patch_len_out] for patch_end in patch_start_indices[1:]
-            ]
-
-            for last_patch_id in range(0, len(patches)):
-                # make last patch ull length + create mask on point level for this patch
-                mask_point_level_final_patch = [False] * len(patches[last_patch_id]) + [True] * (
-                    self.patch_len_in - len(patches[last_patch_id])
+            processed_data.extend(
+                self._process_single_context_window(
+                    time_series[context_start_id : context_start_id + self.max_context_len + self.patch_len_out],
+                    time_start + context_start_id * self.time_units_per_step,
                 )
-                if len(patches[last_patch_id]) < self.patch_len_in:
-                    patches[last_patch_id].extend([0] * (self.patch_len_in - len(patches[last_patch_id])))
-
-                for nr_patches in range(last_patch_id + 1):
-                    # Assemble context from selected patches with padding as necessary
-                    cur_patched_context = patches[last_patch_id - nr_patches : last_patch_id + 1] + [
-                        [0] * self.patch_len_in
-                    ] * (max_nr_patches_per_context_window - nr_patches - 1)
-
-                    # mask first patch on point level
-                    r = random.randint(0, self.patch_len_in - 1)
-                    if nr_patches == 0:
-                        mask_point_level = [[True] * r + mask_point_level_final_patch[r:]] + [
-                            [True] * self.patch_len_in
-                        ] * (max_nr_patches_per_context_window - nr_patches - 1)
-                    else:
-                        mask_point_level = (
-                            [[True] * r + [False] * (self.patch_len_in - r)]
-                            + [[False] * self.patch_len_in] * (nr_patches - 1)
-                            + [mask_point_level_final_patch]
-                            + [[True] * self.patch_len_in] * (max_nr_patches_per_context_window - nr_patches - 1)
-                        )
-                    assert len(mask_point_level) == len(cur_patched_context)
-
-                    # get mask on token level
-                    mask_token_level = [all(m) for m in mask_point_level]
-
-                    # Append processed data entry
-                    # TODO: fix "start" if time feature is ever relevant
-                    processed_data.append(
-                        {
-                            "input": cur_patched_context,
-                            "output": predictions[last_patch_id],
-                            "mask_point_level": mask_point_level,
-                            "mask_token_level": mask_token_level,
-                            "start": time_start,
-                        }
-                    )
+            )
         return processed_data
+
+    def _split_into_patches(self, context_window) -> tuple[list[list[float]], list[list[float]]]:
+        """
+        Split a context window into patches of length `patch_len_in` and subsequent `patch_len_out` points as prediction.
+
+        Args:
+            context_window (list[float]): The context window to split.
+
+        Returns:
+            tuple[list[list[float]], list[list[float]]]: The input patches and the corresponding predictions.
+        """
+        patch_start_indices = [
+            patch_id * self.patch_len_in for patch_id in range(0, self.max_nr_patches_per_context_window)
+        ]
+        patch_start_indices.append(self.max_context_len)
+
+        patches_in = [context_window[start:end] for start, end in pairwise(patch_start_indices)]
+        patches_out = [
+            context_window[patch_end : patch_end + self.patch_len_out] for patch_end in patch_start_indices[1:]
+        ]
+
+        return patches_in, patches_out
+
+    def _process_single_context_window(self, context_window, time_start) -> list[dict]:
+        """Patch a context window, compute masks and return data entries."""
+        processed_data = []
+
+        patches_in, predictions = self._split_into_patches(context_window)
+
+        for nr_patches in range(1, len(patches_in) + 1):
+            cur_patched_context = patches_in[:nr_patches]
+            mask_point_level, delta_start_time = self._create_mask_point_level(nr_patches, cur_patched_context)
+            # pad last patch to full length
+            if len(patches_in[nr_patches - 1]) < self.patch_len_in:
+                patches_in[nr_patches - 1].extend([0] * (self.patch_len_in - len(patches_in[nr_patches - 1])))
+            # pad patch sequence to max_nr_patches_per_context_window
+            cur_patched_context += [[0] * self.patch_len_in] * (self.max_nr_patches_per_context_window - nr_patches)
+            mask_point_level += [[True] * self.patch_len_in] * (self.max_nr_patches_per_context_window - nr_patches)
+
+            mask_token_level = self._create_mask_token_level(mask_point_level)
+
+            processed_data.append(
+                {
+                    "input": cur_patched_context,
+                    "output": predictions[nr_patches - 1],
+                    "mask_point_level": mask_point_level,
+                    "mask_token_level": mask_token_level,
+                    "start": time_start + delta_start_time,
+                }
+            )
+        return processed_data
+
+    def _create_mask_point_level(self, nr_patches, patches_in) -> list[list[bool]]:
+        """
+        Create the mask on point level.
+
+        The first r (random number) points of the first patch are masked out & the last values of last patch if it is not of full length.
+
+        Returns:
+            list[list[bool]]: The mask on point level.
+            datetime.timedelta: The time delta to the start of the first patch (due to masking of first r points in first patch)
+        """
+        # create first patch mask
+        r = random.randint(0, self.patch_len_in - 1)
+        mask_point_level = [[True] * r + [False] * (self.patch_len_in - r)]
+
+        # create patch masks for all patches except the last one
+        if nr_patches > 2:
+            mask_point_level.extend([[False] * self.patch_len_in] * (nr_patches - 2))
+
+        # append padding mask for last patch if necessary
+        if nr_patches > 1:
+            mask_point_level.extend(
+                [[False] * len(patches_in[-1]) + [True] * (self.patch_len_in - len(patches_in[-1]))]
+            )
+
+        delta_start_time = self.time_units_per_step * r
+
+        return mask_point_level, delta_start_time
+
+    def _create_mask_token_level(self, mask_point_level) -> list[bool]:
+        """Create the mask on token level."""
+        return [all(mask) for mask in mask_point_level]
 
     def __str__(self):
         return f"""PatchedDataset(
