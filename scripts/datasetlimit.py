cb11e718e117dd476e0901b8ec8d540734c3ae40
Kostadin Cvejoski <cvejoski@gmail.com>
2024-10-22 10:15:49 +0200

    add ode data

--- scripts/datasetlimit.py
+++ scripts/datasetlimit.py
@@ -1,21 +1,24 @@
-import pickle
 from pathlib import Path
 
 import torch
-from dlpack import asdlpack
 
 
-PATH = "/cephfs_projects/foundation_models/MJP/data/25k_hom_mjp_6_st_10s_1%_noise_rand_300-samples-per-intensity_with_initial_distribution/test/test/"
+PATH = "/cephfs_projects/foundation_models/data/torch_2M_ode_chebyshev_max_deg_100_rbf_gp_2_5_and_2_10_length_128_avg_min_8/"
 
-OUT_PATH = "/home/cvejoski/Projects/FoundationModels/FIM/tests/resources/data/mjp/25k_hom_mjp_6_st_10s_1_noise_rand_300-samples-per-intensity_with_initial_distribution/test/"
+OUT_PATH = "/home/cvejoski/Projects/FoundationModels/FIM/tests/resources/data/ode/torch_2M_ode_chebyshev_max_deg_100_rbf_gp_2_5_and_2_10_length_128_avg_min_8/"
 OUT_PATH = Path(OUT_PATH)
 OUT_PATH.mkdir(parents=True, exist_ok=True)
-for file in Path(PATH).rglob("*.pickle"):
+for file in Path(PATH).rglob("*.pt"):
     with open(file, "rb") as f:
-        data = pickle.load(f)
+        print(file)
+        data = torch.load(f)
         B = 2  # Set the limit for the first dimension
-        limited_data = data[:B]
-        limited_data = torch.from_dlpack(asdlpack(limited_data))
+        limited_data = {}
+        for k, v in data.items():
+            limited_data[k] = torch.clone(v[:B])
+        # limited_data = torch.from_dlpack(asdlpack(limited_data))
+        for k, v in limited_data.items():
+            print(k, v.shape)
         torch.save(limited_data, str(OUT_PATH) + "/" + file.stem + ".pt")
         # with open(str(OUT_PATH) + "/" + file.name, "wb") as f_out:
         #     pickle.dump(limited_data, f_out)
