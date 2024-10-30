fix resume train, change the calculation of the mean of the loss by considering the zero entries

src/fim/models/mjp.py
@@ -170,23 +170,26 @@ class FIMMJP(AModel):
         return out
 
     def __decode(self, h: Tensor) -> tuple[Tensor, Tensor]:
-        pred_offdiag_im_logvar = self.intensity_matrix_decoder(h)
+        pred_offdiag_mean_logvar = self.intensity_matrix_decoder(h)
         init_cond = self.initial_distribution_decoder(h)
-        return pred_offdiag_im_logvar, init_cond
+        return pred_offdiag_mean_logvar, init_cond
 
     def __encode(self, x: Tensor, obs_grid_normalized: Tensor, obs_values_one_hot: Tensor) -> Tensor:
         B, P, L = obs_grid_normalized.shape[:3]
         pos_enc = self.pos_encodings(obs_grid_normalized)
-        path = torch.cat([pos_enc, obs_values_one_hot], dim=-1)  # [t, delta_t]
+        path = torch.cat([pos_enc, obs_values_one_hot], dim=-1)
         if isinstance(self.ts_encoder, TransformerEncoder):
             padding_mask = create_padding_mask(x["mask_seq_lengths"].view(B * P), L)
             padding_mask[:, 0] = True
-            h = self.ts_encoder(path.view(B * P, L, -1), padding_mask)[:, -1, :].view(B, P, -1)
-            h = self.path_attention(h, h, h)[0][:, 1, :]
+            h = self.ts_encoder(path.view(B * P, L, -1), padding_mask)[:, 1, :].view(B, P, -1)
+            if isinstance(self.path_attention, nn.MultiheadAttention):
+                h = self.path_attention(h, h, h)[0][:, -1]
+            else:
+                h = self.path_attention(h, h, h)
         elif isinstance(self.ts_encoder, RNNEncoder):
             h = self.ts_encoder(path.view(B * P, L, -1), x["mask_seq_lengths"].view(B * P))
             last_observation = x["mask_seq_lengths"].view(B * P) - 1
-            h = h[torch.arange(B * P), last_observation, :].view(B, P, -1)
+            h = h[torch.arange(B * P), last_observation].view(B, P, -1)
             h = self.path_attention(h, h, h)
 
         h = torch.cat([h, torch.ones(B, 1).to(h.device) / 100.0 * P], dim=-1)
@@ -221,10 +224,11 @@ class FIMMJP(AModel):
         target_mean = get_off_diagonal_elements(target_im)
         adjaceny_matrix = get_off_diagonal_elements(adjaceny_matrix)
         target_init_cond = torch.argmax(target_init_cond, dim=-1).long()
-
-        loss_gauss = adjaceny_matrix * self.gaussian_nll(pred_im, target_mean, torch.exp(pred_logvar_im))
+        pred_im_var = torch.exp(pred_logvar_im)
+        loss_gauss = adjaceny_matrix * self.gaussian_nll(pred_im, target_mean, pred_im_var)
         loss_initial = self.init_cross_entropy(pred_init_cond, target_init_cond)
-        loss_missing_link = normalization_constants * (1.0 - adjaceny_matrix) * (torch.pow(pred_im, 2) + torch.exp(pred_logvar_im))
+        zero_entries = 1.0 - adjaceny_matrix
+        loss_missing_link = normalization_constants * zero_entries * (torch.pow(pred_im, 2) + pred_im_var)
         rmse_loss = torch.sqrt(torch.mean((target_mean - pred_im) ** 2))
 
         gaus_cons = schedulers.get("gauss_nll")(step) if schedulers else torch.tensor(1.0)
@@ -237,10 +241,10 @@ class FIMMJP(AModel):
         loss = gaus_cons * loss_gauss + init_cons * loss_initial.view(-1, 1) + missing_link_cons * loss_missing_link
 
         return {
-            "loss": loss.mean(),
-            "loss_gauss": loss_gauss.mean(),
+            "loss": loss.sum(),
+            "loss_gauss": loss_gauss.sum() / adjaceny_matrix.sum(),
             "loss_initial": loss_initial.mean(),
-            "loss_missing_link": loss_missing_link.mean(),
+            "loss_missing_link": loss_missing_link.sum() / zero_entries.sum(),
             "rmse_loss": rmse_loss,
             "beta_gauss_nll": gaus_cons,
             "beta_init_cross_entropy": init_cons,

src/fim/trainers/checkpoint.py
@@ -68,6 +68,7 @@ class TrainCheckpoint:
         grad_scaler: GradScaler = None,
         rank: int = 0,
         is_peft: bool = False,
+        resume_dir: Path | str = None,
     ):
         """
         Initializes the TrainCheckpoint instance.
@@ -93,6 +94,7 @@ class TrainCheckpoint:
         self.checkpoint_dir = Path(experiment_dir) / "checkpoints"
         self.best_model_dir = self.checkpoint_dir / "best-model"
         self.is_peft = is_peft
+        self.resume_dir = resume_dir
         if self.rank != 0:
             return
         self.__logger.info("Initializing Checkpoint Directories ...")
@@ -313,11 +315,11 @@ class TrainCheckpoint:
 
     def _get_checkpoint_path(self, checkpoint: Union[int, Literal["best-model", "last-epoch"]]) -> Path:
         if checkpoint == "best-model":
-            checkpoint_path = self.checkpoint_dir / "best-model"
+            checkpoint_path = self.resume_dir / "best-model"
         elif checkpoint == "last-epoch":
             checkpoint_path = self.__get_last_epoch()
         else:
-            checkpoint_path = self.checkpoint_dir / f"epoch-{checkpoint}"
+            checkpoint_path = self.resume_dir / f"epoch-{checkpoint}"
 
         if not checkpoint_path.exists():
             raise FileNotFoundError(f"Checkpoint '{checkpoint_path}' not found")
@@ -325,13 +327,13 @@ class TrainCheckpoint:
 
     def __get_last_epoch(self):
         epoch_numbers = []
-        for checkpoint_name in [item.name for item in self.checkpoint_dir.iterdir() if item.is_dir()]:
+        for checkpoint_name in [item.name for item in self.resume_dir.iterdir() if item.is_dir()]:
             match = re.match(r"^epoch-(\d+)$", checkpoint_name)
             if match:
                 epoch_numbers.append(int(match.group(1)))
         if not epoch_numbers:
-            raise FileNotFoundError(f"Checkpoints not found in '{self.checkpoint_dir}' not found")
-        return self.checkpoint_dir / f"epoch-{max(epoch_numbers)}"
+            raise FileNotFoundError(f"Checkpoints not found in '{self.resume_dir}' not found")
+        return self.resume_dir / f"epoch-{max(epoch_numbers)}"
 
     def state_dict(self) -> dict:
         return self.best_model_flag
@@ -510,14 +512,14 @@ class TrainCheckpointFSDPFullStateDict(TrainCheckpoint):
         grad_scaler_state = None
         if self.rank == 0:
             self.__logger.info("Loading Optimizers State from %s ...", file_name)
-            checkpoint_ = torch.load(file_name)  # Load checkpoint on CPU
+            checkpoint_ = torch.load(file_name, weights_only=True)  # Load checkpoint on CPU
             grad_scaler_state = checkpoint_["grad_scaler"]
         grad_scaler_state = broadcast_state_dict(grad_scaler_state, "grad_scaler")
         dist.barrier()
         if self.grad_scaler is not None and grad_scaler_state is not None:
             self.grad_scaler.load_state_dict(grad_scaler_state)
         for name, optimizer in self.optimizers.items():
-            schedulers_sd = [None] * len(optimizer["schedulers"])
+            schedulers_sd = [None] * len(optimizer["schedulers"]) if optimizer["schedulers"] is not None else None
             full_osd = None
             if name in checkpoint_:
                 full_osd = checkpoint_[name]["opt"]
@@ -525,11 +527,12 @@ class TrainCheckpointFSDPFullStateDict(TrainCheckpoint):
             sharded_osd = FSDP.scatter_full_optim_state_dict(full_osd, self.model)
             optimizer["opt"].load_state_dict(sharded_osd)
             self.__logger.info("Optimizer %s Loaded!", name)
-            for ix, scheduler_sd in enumerate(schedulers_sd):
-                scheduler_sd = broadcast_state_dict(scheduler_sd, "scheduler_state_dict")
-                # self.__logger.debug("Recieved Scheduler state for optimizer '%s': %s!", name, schedulers_sd)
-                optimizer["schedulers"][ix][1].load_state_dict(scheduler_sd)
-            self.__logger.info("Optimizer-Schedulers %s Loaded!", name)
+            if schedulers_sd is not None:
+                for ix, scheduler_sd in enumerate(schedulers_sd):
+                    scheduler_sd = broadcast_state_dict(scheduler_sd, "scheduler_state_dict")
+                    # self.__logger.debug("Recieved Scheduler state for optimizer '%s': %s!", name, schedulers_sd)
+                    optimizer["schedulers"][ix][1].load_state_dict(scheduler_sd)
+                self.__logger.info("Optimizer-Schedulers %s Loaded!", name)
 
 
 non_reentrant_wrapper = partial(

src/fim/trainers/trainer.py
@@ -31,7 +31,7 @@ from .utils import (
 
 
 class Trainer:
-    def __init__(self, model: AModel, dataloader: BaseDataLoader, config: GenericConfig, resume: bool = False) -> None:
+    def __init__(self, model: AModel, dataloader: BaseDataLoader, config: GenericConfig, resume: Optional[Path | str] = None) -> None:
         self.config = config
         self.resume = resume
         self.logger = RankLoggerAdapter(logging.getLogger(self.__class__.__name__))
@@ -49,7 +49,7 @@ class Trainer:
         self._save_experiment_parameters()
         torch.autograd.set_detect_anomaly(self.config.trainer.detect_anomaly)
 
-    def _prepare_model(self, model, config, resume: Optional[Path] = None):
+    def _prepare_model(self, model, config, resume: Optional[Path | str] = None):
         self.model = model
         checkpoint_class = TrainCheckpointFSDPFullStateDict if self.is_distributed else TrainCheckpoint
         self.optimizers = create_optimizers(self.model, config.optimizers)
@@ -65,6 +65,7 @@ class Trainer:
             grad_scaler=self.grad_scaler,
             rank=self.rank,
             is_peft=False,  # self.model.is_peft(),
+            resume_dir=resume,
         )
         if config.experiment.device_map == "auto":
             device = torch.device(self.accel_type)
@@ -90,7 +91,6 @@ class Trainer:
             if resume is not None:
                 self.checkpointer.load_optimizers_state("last-epoch")
                 self.optimizers = self.checkpointer.optimizers
-            # dist.barrier()
 
     def _setup_variables(self):
         self.is_accelerator = is_accelerator_available()
@@ -170,11 +170,8 @@ class Trainer:
 
     def _prepare_experiment_dirs(self) -> None:
         name = self.config.experiment.name
-        if self.resume is not None:
-            # remove "-seed-n" at end
-            name = name[: name.rfind("-experiment-seed-")]
-            date = ""
-        elif self.config.experiment.name_add_date:
+
+        if self.config.experiment.name_add_date:
             # add date as string if wanted
             date = "_" + datetime.now().strftime("%m-%d-%H%M")
         else: