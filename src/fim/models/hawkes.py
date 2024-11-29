ae06bb388688b049f89a1d6cb130181c29215aa6
Kostadin Cvejoski <cvejoski@gmail.com>
2024-11-29 14:37:36 +0100

    add logger attribute in Hawkes model

--- src/fim/models/hawkes.py
+++ src/fim/models/hawkes.py
@@ -1,15 +1,16 @@
 import copy
+import logging
 from typing import Any, Dict
 
 import torch
 from torch import Tensor, nn
 from transformers import AutoConfig, AutoModel, PretrainedConfig
 
-from ..utils.helper import create_class_instance
-from .blocks import AModel, ModelFactory, RNNEncoder, TransformerEncoder
-from .blocks.positional_encodings import SineTimeEncoding
-from .utils import create_matrix_from_off_diagonal, create_padding_mask, get_off_diagonal_elements
 from fim.models.blocks import MultiHeadLearnableQueryAttention
+
+from ..utils.helper import create_class_instance
+from ..utils.logging import RankLoggerAdapter
+from .blocks import AModel, ModelFactory, RNNEncoder
 
 
 class FIMHawkesConfig(PretrainedConfig):
@@ -44,12 +45,13 @@ class FIMHawkesConfig(PretrainedConfig):
 
         super().__init__(**kwargs)
 
+
 class FIMHawkes(AModel):
     """
     FIMHawkes: A Neural Recognition Model for Zero-Shot Inference of Hawkes Processes
 
-    Attributes:        
-        num_marks (int): Number of marks in the Hawkes process.        
+    Attributes:
+        num_marks (int): Number of marks in the Hawkes process.
         ts_encoder (dict | TransformerEncoder): Time series encoder.
         time_encodings (dict | SineTimeEncoding): Time encodings.
         event_type_embedding (dict | nn.Module): Event type embedding.
@@ -78,21 +80,28 @@ class FIMHawkes(AModel):
         metric(y: Any, y_target: Any) -> Dict:
             Compute the metric for the model.
     """
+
     config_class = FIMHawkesConfig
-    
+
     def __init__(self, config: FIMHawkesConfig, **kwargs):
         super().__init__(config, **kwargs)
         self.num_marks = config.num_marks
-
+        self.logger = RankLoggerAdapter(logging.getLogger(self.__class__.__name__))
         self.__create_modules()
 
         # self.gaussian_nll = nn.GaussianNLLLoss(full=True, reduction="none")
         # self.init_cross_entropy = nn.CrossEntropyLoss(reduction="none")
-        
+
         assert isinstance(self.Omega_1_encoder, nn.MultiheadAttention), "Omega_1_encoder must be an instance of nn.MultiheadAttention"
-        assert isinstance(self.Omega_2_encoder, MultiHeadLearnableQueryAttention), "Omega_2_encoder must be an instance of MultiHeadLearnableQueryAttention"
-        assert isinstance(self.Omega_3_encoder, MultiHeadLearnableQueryAttention), "Omega_3_encoder must be an instance of MultiHeadLearnableQueryAttention"
-        assert isinstance(self.Omega_4_encoder, MultiHeadLearnableQueryAttention), "Omega_4_encoder must be an instance of MultiHeadLearnableQueryAttention"
+        assert isinstance(
+            self.Omega_2_encoder, MultiHeadLearnableQueryAttention
+        ), "Omega_2_encoder must be an instance of MultiHeadLearnableQueryAttention"
+        assert isinstance(
+            self.Omega_3_encoder, MultiHeadLearnableQueryAttention
+        ), "Omega_3_encoder must be an instance of MultiHeadLearnableQueryAttention"
+        assert isinstance(
+            self.Omega_4_encoder, MultiHeadLearnableQueryAttention
+        ), "Omega_4_encoder must be an instance of MultiHeadLearnableQueryAttention"
 
     def __create_modules(self) -> None:
         ts_encoder = copy.deepcopy(self.config.ts_encoder)
@@ -105,31 +114,30 @@ class FIMHawkes(AModel):
         Omega_4_encoder = copy.deepcopy(self.config.Omega_4_encoder)
         kernel_value_decoder = copy.deepcopy(self.config.kernel_value_decoder)
         kernel_parameter_decoder = copy.deepcopy(self.config.kernel_parameter_decoder)
-        
+
         self.time_encodings = create_class_instance(time_encodings.pop("name"), time_encodings)
-        
+
         self.event_type_embedding = create_class_instance(event_type_embedding.pop("name"), event_type_embedding)
-        
+
         ts_encoder["in_features"] = self.time_encodings.out_features + self.event_type_embedding.out_features
         self.ts_encoder = create_class_instance(ts_encoder.pop("name"), ts_encoder)
-        
+
         trunk_net["in_features"] = self.time_encodings.out_features
         self.trunk_net = create_class_instance(trunk_net.pop("name"), trunk_net)
-        
+
         self.Omega_1_encoder = create_class_instance(Omega_1_encoder.pop("name"), Omega_1_encoder)
-        
+
         self.Omega_2_encoder = create_class_instance(Omega_2_encoder.pop("name"), Omega_2_encoder)
-        
+
         self.Omega_3_encoder = create_class_instance(Omega_3_encoder.pop("name"), Omega_3_encoder)
-        
+
         self.Omega_4_encoder = create_class_instance(Omega_4_encoder.pop("name"), Omega_4_encoder)
-        
+
         kernel_value_decoder["out_features"] = self.num_marks
         self.kernel_value_decoder = create_class_instance(kernel_value_decoder.pop("name"), kernel_value_decoder)
-        
-        kernel_parameter_decoder["out_features"] = 2*self.num_marks
+
+        kernel_parameter_decoder["out_features"] = 2 * self.num_marks
         self.kernel_parameter_decoder = create_class_instance(kernel_parameter_decoder.pop("name"), kernel_parameter_decoder)
-        
 
     def forward(self, x: dict[str, Tensor], schedulers: dict = None, step: int = None) -> dict:
         """
@@ -153,7 +161,7 @@ class FIMHawkes(AModel):
                 - "losses" (optional): Tensor representing the calculated losses, if the required keys are present in `x`.
         """
         x["observation_values_one_hot"] = torch.nn.functional.one_hot(x["event_types"].long().squeeze(-1), num_classes=self.num_marks)
-        
+
         obs_grid = x["event_times"]
         if "time_normalization_factors" not in x:
             norm_constants, obs_grid = self.__normalize_obs_grid(obs_grid)
@@ -163,29 +171,29 @@ class FIMHawkes(AModel):
             norm_constants = x["time_normalization_factors"]
             x["observation_grid_normalized"] = obs_grid
 
-        #FIXME: REMOVE THIS!
+        # FIXME: REMOVE THIS!
         x["kernel_grids"] = x["kernel_grids"][:, :, ::10]
-        x["kernel_evaluations"] = x["kernel_evaluations"][:,:,::10]
-        print("WARNING: Kernel grids and evaluations are truncated to 10")
+        x["kernel_evaluations"] = x["kernel_evaluations"][:, :, ::10]
+        self.logger.warning("Kernel grids and evaluations are truncated to 10!")
 
-        sequence_encodings = self.__encode_observations(x) # [B, P, L, D]
-            
-        trunk_net_encodings = self.__trunk_net_encoder(x) # [B, M, L_kernel, D]
-        
-        time_dependent_path_embeddings = self.__Omega_1_encoder(x, trunk_net_encodings, sequence_encodings) # [B, M, L_kernel, P, D_1]
-        
-        static_path_embeddings = self.__Omega_2_encoder(sequence_encodings) # [B, P, D_2]
-        
-        time_dependent_path_summary = self.__Omega_3_encoder(time_dependent_path_embeddings) # [B, M, L_kernel, D_3]
-        
-        static_path_summary = self.__Omega_4_encoder(static_path_embeddings) # [B, D_4]
-        
-        predicted_kernel_values = self.__kernel_value_decoder(time_dependent_path_summary).squeeze() # [B, M, L_kernel]
-        
-        predicted_kernel_decay_and_base_intensity = torch.exp(self.__kernel_parameter_decoder(static_path_summary)) # [B, M, 2]
-        predicted_base_intensity = predicted_kernel_decay_and_base_intensity[:,:,0]
-        predicted_kernel_decay = predicted_kernel_decay_and_base_intensity[:,:,1]
-        
+        sequence_encodings = self.__encode_observations(x)  # [B, P, L, D]
+
+        trunk_net_encodings = self.__trunk_net_encoder(x)  # [B, M, L_kernel, D]
+
+        time_dependent_path_embeddings = self.__Omega_1_encoder(x, trunk_net_encodings, sequence_encodings)  # [B, M, L_kernel, P, D_1]
+
+        static_path_embeddings = self.__Omega_2_encoder(sequence_encodings)  # [B, P, D_2]
+
+        time_dependent_path_summary = self.__Omega_3_encoder(time_dependent_path_embeddings)  # [B, M, L_kernel, D_3]
+
+        static_path_summary = self.__Omega_4_encoder(static_path_embeddings)  # [B, D_4]
+
+        predicted_kernel_values = self.__kernel_value_decoder(time_dependent_path_summary).squeeze()  # [B, M, L_kernel]
+
+        predicted_kernel_decay_and_base_intensity = torch.exp(self.__kernel_parameter_decoder(static_path_summary))  # [B, M, 2]
+        predicted_base_intensity = predicted_kernel_decay_and_base_intensity[:, :, 0]
+        predicted_kernel_decay = predicted_kernel_decay_and_base_intensity[:, :, 1]
+
         out = {
             "predicted_kernel_values": predicted_kernel_values,
             "predicted_base_intensity": predicted_base_intensity,
@@ -201,19 +209,18 @@ class FIMHawkes(AModel):
                 schedulers,
                 step,
             )
-        
+
         return out
-        
 
     def __encode_observations(self, x: dict) -> Tensor:
         obs_grid_normalized = x["observation_grid_normalized"]
         obs_values_one_hot = x["observation_values_one_hot"]
         B, P, L = obs_grid_normalized.shape[:3]
-        
-        #FIXME: Do this inside the dataloader
+
+        # FIXME: Do this inside the dataloader
         x["seq_lengths"] = torch.tensor([L] * B * P, device=self.device)
         x["seq_lengths"] = x["seq_lengths"].view(B, P)
-        
+
         time_enc = self.time_encodings(obs_grid_normalized)
         state_enc = self.event_type_embedding(obs_values_one_hot)
         path = torch.cat([time_enc, state_enc], dim=-1)
@@ -223,13 +230,13 @@ class FIMHawkes(AModel):
         # h = h[torch.arange(B * P), last_observation].view(B, P, -1)
 
         return h.view(B, P, L, -1)
-    
+
     def __trunk_net_encoder(self, x: dict) -> Tensor:
-        kernel_grids = x["kernel_grids"] #TODO: Dont work with the full grid
+        kernel_grids = x["kernel_grids"]  # TODO: Dont work with the full grid
         (B, M, L_kernel) = kernel_grids.shape
-        time_encodings = self.time_encodings(kernel_grids.reshape(B*M*L_kernel, -1))
+        time_encodings = self.time_encodings(kernel_grids.reshape(B * M * L_kernel, -1))
         return self.trunk_net(time_encodings).view(B, M, L_kernel, -1)
-    
+
     def __Omega_1_encoder(self, x: dict, trunk_net_encoding: Tensor, observation_encoding: Tensor) -> Tensor:
         """
         The time-dependent path embeddings with variable sequence lengths.
@@ -270,7 +277,7 @@ class FIMHawkes(AModel):
         # key_padding_mask: (batch_size, seq_length)
         # Here, batch_size = B * M * L_kernel * P
         mask = torch.arange(L, device=seq_lengths.device).unsqueeze(0).expand(B * M * L_kernel * P, L) >= seq_lengths.unsqueeze(1)
-        
+
         # Apply encoder with key_padding_mask
         encoder_output, _ = self.Omega_1_encoder(queries, keys, values, key_padding_mask=mask)
         # Select the last valid output based on seq_lengths
@@ -279,40 +286,40 @@ class FIMHawkes(AModel):
         h = encoder_output[torch.arange(encoder_output.size(0)), last_indices]
 
         return h.view(B, M, L_kernel, P, -1)
-    
+
     def __Omega_2_encoder(self, sequence_encodings: Tensor) -> Tensor:
         """
         The static path embeddings.
         """
         B, P, L, D = sequence_encodings.shape
-        sequence_encodings = sequence_encodings.view(B*P, L, D)
+        sequence_encodings = sequence_encodings.view(B * P, L, D)
         h = self.Omega_2_encoder(None, sequence_encodings, sequence_encodings)
-        
+
         return h.view(B, P, -1)
-    
+
     def __Omega_3_encoder(self, time_dependent_path_embeddings: Tensor) -> Tensor:
         """
         The time dependent path summary.
         """
         B, M, L_kernel, P, D_1 = time_dependent_path_embeddings.shape
-        time_dependent_path_embeddings = time_dependent_path_embeddings.view(B*M*L_kernel, P, D_1)
+        time_dependent_path_embeddings = time_dependent_path_embeddings.view(B * M * L_kernel, P, D_1)
         h = self.Omega_3_encoder(None, time_dependent_path_embeddings, time_dependent_path_embeddings)
-        
+
         return h.view(B, M, L_kernel, -1)
-    
+
     def __Omega_4_encoder(self, static_path_embeddings: Tensor) -> Tensor:
         """
         The static path summary.
         """
         return self.Omega_4_encoder(None, static_path_embeddings, static_path_embeddings)
-    
+
     def __kernel_value_decoder(self, time_dependent_path_summary: Tensor) -> Tensor:
         B, M, L_kernel, D_3 = time_dependent_path_summary.shape
-        time_dependent_path_summary = time_dependent_path_summary.view(B*M*L_kernel, D_3)
+        time_dependent_path_summary = time_dependent_path_summary.view(B * M * L_kernel, D_3)
         h = self.kernel_value_decoder(time_dependent_path_summary)
-        
+
         return h.view(B, M, L_kernel, -1)
-    
+
     def __kernel_parameter_decoder(self, static_path_summary: Tensor) -> Tensor:
         h = self.kernel_parameter_decoder(static_path_summary)
         return h.view(-1, self.num_marks, 2)
@@ -333,15 +340,15 @@ class FIMHawkes(AModel):
         step: int = None,
     ) -> dict:
         predicted_kernel_function = predicted_kernel_values * torch.exp(-predicted_kernel_decay.unsqueeze(-1))
-        
+
         kernel_rmse = torch.sqrt(torch.mean((predicted_kernel_function - target_kernel_values) ** 2))
         base_intensity_rmse = torch.sqrt(torch.mean((predicted_base_intensity - target_base_intensity) ** 2))
-        
-        # print("Prediction", predicted_kernel_function)  
-        # print("Target", target_kernel_values)      
-        
+
+        # print("Prediction", predicted_kernel_function)
+        # print("Target", target_kernel_values)
+
         loss_1 = kernel_rmse + base_intensity_rmse
-        
+
         loss = loss_1
 
         return {
