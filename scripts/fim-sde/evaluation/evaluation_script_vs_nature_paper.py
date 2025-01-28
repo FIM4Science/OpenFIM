a6743218518add3fc5d36e98115b82015e99b652
David Berghaus <machs3ll@gmail.com>
2025-01-28 18:56:50 +0100

    Updated mmd evaluation script for new data

--- scripts/fim-sde/evaluation/evaluation_script_vs_nature_paper.py
+++ scripts/fim-sde/evaluation/evaluation_script_vs_nature_paper.py
@@ -1,53 +1,91 @@
 from dataclasses import dataclass
+from datetime import datetime
 from pathlib import Path
 import pickle
 from tabulate import tabulate
 import torch
+import json
 
 from fim.data.utils import load_h5s_in_folder
 from fim.utils.plots.fim_sde_quiver_plots import create_2D_quiver_plot
 
+from compute_mmd import get_mmd
 
-
-class Dataset:
+class ModelResults:
     def __init__(self, name: str, path: Path, **plot_kwargs):
         self.name = name
         self.path = path
         
-        if str(path).endswith(".pickle"):
-            with open(path, "rb") as f:
-                data = pickle.load(f).results
-            self.mean_path = data["sample_paths"].mean(dim=1)[...,:2]
-            self.grid = data["sample_paths_grid"][:,:1,...]
-        else:
-            data = load_h5s_in_folder(path)
-            self.mean_path = data["obs_values"][0] # actually thats the ode solution
-            self.grid = data["obs_times"][0]
-            
-        # Make sure that data is on cpu
-        self.mean_path = self.mean_path.cpu()
-        self.grid = self.grid.cpu()
+        if str(path).endswith(".json"):
+            with open(path, "r") as f:
+                data = json.load(f)
+        self.data = data
     
 
 
 @dataclass
 class Benchmark:
-    name: str
-    GroundTruth: Dataset
-    FIM: Dataset
-    ComparisonModel: Dataset = None
+    FIM_11M: ModelResults
+    FIM_20M: ModelResults
+    BISDE: ModelResults
+    SparseGP: ModelResults
     
 
-def run_benchmark_and_plot(benchmark: Benchmark):
-    assert torch.allclose(benchmark.GroundTruth.grid, benchmark.FIM.grid), "Grids do not match"
-    ground_truth_mean = benchmark.GroundTruth.mean_path
-    model_mean = benchmark.FIM.mean_path
-    
-    table = [["FIM",nrmse(model_mean, ground_truth_mean)]]
-    
-    print(f"Results for {benchmark.name}")
-    print(tabulate(table, headers=["Model", "NRMSE Mean Path"]))
-    print("\n")
+def get_dataset_idx(data, dataset_name):
+    """
+    Fuck me that I have to write this function...
+    """
+    data_names = [d["name"] for d in data]
+    if 'Glycosis' in data_names:
+        data_names[data_names.index('Glycosis')] = "Glycolysis"
+    if 'Syn Drift' in data_names:
+        data_names[data_names.index('Syn Drift')] = "SynDrift"
+    if 'Double Well Max Diffusion' in data_names:
+        data_names[data_names.index('Double Well Max Diffusion')] = "DoubleWell"
+    if 'Damped Linear' in data_names:
+        data_names[data_names.index('Damped Linear')] = "DampedLinear"
+    if 'Damped Cubic' in data_names:
+        data_names[data_names.index('Damped Cubic')] = "DampedCubic"
+    if dataset_name not in data_names:
+        raise ValueError(f"Dataset {dataset_name} not found in {data_names}")
+    return data_names.index(dataset_name)
+
+
+def throw_away_nan_paths(estimated_paths, ground_truth_paths, dataset_name="", model_name=""):
+    original_num_paths = estimated_paths.shape[0]
+    # Detect which rows contain NaNs in estimated_paths
+    valid_idx = torch.tensor([i for i in range(original_num_paths) if not torch.isnan(estimated_paths[i]).any()], dtype=torch.long)
+    estimated_paths = estimated_paths[valid_idx]
+    ground_truth_paths = ground_truth_paths[valid_idx]
+    if len(valid_idx) < original_num_paths:
+        print(f"Warning: {len(estimated_paths) - len(valid_idx)} NaN paths in {dataset_name} for {model_name}")
+    return estimated_paths, ground_truth_paths    
+
+
+def run_benchmark(benchmark: Benchmark, evaluation_dir):
+    with open(evaluation_dir / "results.txt","w") as f: 
+        for dataset_name in ['Duffing', 'Glycolysis', 'SynDrift', 'Wang', 'DoubleWell', 'DampedLinear', 'DampedCubic']:
+            table = []
+            for model_name in ['FIM_11M', 'FIM_20M', 'BISDE', 'SparseGP']:
+                model = getattr(benchmark, model_name)
+                dataset_idx = get_dataset_idx(model.data, dataset_name)
+                data = model.data[dataset_idx]
+                # drift_rmse = rmse(data["estimated_drift_at_locations"], data["real_drift_at_locations"])
+                # diffusion_rmse = rmse(data["estimated_diffusion_at_locations"], data["real_diffusion_at_locations"])
+                ground_truth_paths = torch.tensor(data["real_paths"])
+                estimated_paths = torch.tensor(data["synthetic_paths"])
+                if not torch.isnan(estimated_paths).any():
+                    # mean_path_rmse = rmse(estimated_paths.mean(dim=0), ground_truth_paths.mean(dim=0))
+                    mmd = get_mmd(estimated_paths, ground_truth_paths)
+                    table.append([model_name, mmd])
+        
+            print(f"Results for {dataset_name}")
+            print(tabulate(table, headers=["Model", "MMD"]))
+            print("\n")
+            
+            f.write(f"Results for {dataset_name}\n")
+            f.write(tabulate(table, headers=["Model", "MMD"]))
+            f.write("\n\n")
     
 def rmse(m_pred, y_true):
    return (m_pred - y_true).pow(2).mean().sqrt()
@@ -56,82 +94,20 @@ def nrmse(m_pred, y_true):
     return rmse(m_pred, y_true) / rmse(torch.zeros_like(y_true), y_true)
 
 if __name__ == "__main__":
+    # Save dir setup: project_path / evaluations / synthetic_datasets / time_stamp + _ + experiment_descr
+    evaluation_path =  "evaluations"
+    time: str = str(datetime.now().strftime("%m%d%H%M"))
+    evaluation_dir: Path = Path(f"evaluations/ksig/{time}")
+    evaluation_dir.mkdir(parents=True, exist_ok=True)
+    
     benchmarks = [
         Benchmark(
-            name="Damped Cubic Oscillator",
-            GroundTruth = Dataset("Ground Truth", Path("/cephfs_projects/foundation_models/data/SDE/test/20250124_svise/ode/damped_cubic_oscillator")),
-            FIM = Dataset("FIM", Path("evaluations/nature_datasets/01271243_develop/model_evaluations/11M_params/svise_damped_cubic/default11M_params_svise_damped_cubic.pickle")),
-        ),
-        Benchmark(
-            name="Damped Linear Oscillator",
-            GroundTruth = Dataset("Ground Truth", Path("/cephfs_projects/foundation_models/data/SDE/test/20250124_svise/ode/damped_linear_oscillator")),
-            FIM = Dataset("FIM", Path("evaluations/nature_datasets/01271243_develop/model_evaluations/11M_params/svise_damped_linear/default11M_params_svise_damped_linear.pickle")),
-        ),
-        Benchmark(
-            name="Duffing Oscillator",
-            GroundTruth = Dataset("Ground Truth", Path("/cephfs_projects/foundation_models/data/SDE/test/20250124_svise/ode/duffing_oscillator")),
-            FIM = Dataset("FIM", Path("evaluations/nature_datasets/01271243_develop/model_evaluations/11M_params/svise_duffing/default11M_params_svise_duffing.pickle")),
-        ),
-        Benchmark(
-            name="Hopf Bifurcation",
-            GroundTruth = Dataset("Ground Truth", Path("/cephfs_projects/foundation_models/data/SDE/test/20250124_svise/ode/hopf_bifurcation")),
-            FIM = Dataset("FIM", Path("evaluations/nature_datasets/01271243_develop/model_evaluations/11M_params/svise_hopf_bifurcation/default11M_params_svise_hopf_bifurcation.pickle")),
-        ),
-        Benchmark(
-            name="Selkov Glycolysis",
-            GroundTruth = Dataset("Ground Truth", Path("/cephfs_projects/foundation_models/data/SDE/test/20250124_svise/ode/selkov_glycolysis")),
-            FIM = Dataset("FIM", Path("evaluations/nature_datasets/01271243_develop/model_evaluations/11M_params/svise_selkov_glycolysis/default11M_params_svise_selkov_glycolysis.pickle")),
+            FIM_11M = ModelResults("FIM 11M", Path("data/processed/test/paths/fim_11M_params_stride_1_observations_many_paths.json")),
+            FIM_20M = ModelResults("FIM 20M", Path("data/processed/test/paths/fim_20M_params_trained_even_longer_stride_10_many_paths.json")),
+            BISDE = ModelResults("BISDE", Path("data/processed/test/paths/bisde_dense_observations_paths.json")),
+            SparseGP = ModelResults("SparseGP", Path("data/processed/test/paths/sparse_gp_dense_observations_many_paths.json")),
         ),
     ]
     
     for benchmark in benchmarks:
-        run_benchmark_and_plot(benchmark)
-    
-
-# SVISE Log10 RMSE Medians    
-# -1.5177725118483412
-# -1.5225118483412323
-# -1.3412322274881516
-# -1.5367298578199051
-# -1.9466824644549763
-# -1.3554502369668247
-
-# SVISE Log10 RMSE Lower bound    
-# -1.6171293161814488
-# -1.7095463777928233
-# -1.4524373730534867
-# -1.7853757616790793
-# -2.0815842924847665
-# -1.6467501692620177
-
-# SVISE Log10 RMSE Upper bound
-# -1.390825998645904
-# -1.310257278266757
-# -1.2664184157075153
-# -1.1846648612051456
-# -1.846987136086662
-# -1.2154705484089372
-
-# PF Log10 RMSE Medians
-# -1.4595463777928233
-# -1.3209207853757616
-# -1.5045700744752877
-# -1.4927217332430602
-# -1.772342586323629
-# -1.3422477995937712
-
-# PF Log10 RMSE Lower bound
-# -1.5815842924847665
-# -1.4631008801624916
-# -1.6135748138117807
-# -1.6538591740013542
-# -1.8825321597833446
-# -1.4571767095463777
-
-# PF Log10 RMSE Upper bound
-# -1.367298578199052
-# -1.2097156398104265
-# -1.4277251184834123
-# -1.1623222748815167
-# -1.6670616113744077
-# -1.1812796208530805
+        run_benchmark(benchmark, evaluation_dir)
