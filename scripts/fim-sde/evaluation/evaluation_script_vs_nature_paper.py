e8a93da73892625ca8d446ee36d74ff91682bd36
David Berghaus <machs3ll@gmail.com>
2025-01-29 17:36:03 +0100

    Updated script for yet again another format

--- scripts/fim-sde/evaluation/evaluation_script_vs_nature_paper.py
+++ scripts/fim-sde/evaluation/evaluation_script_vs_nature_paper.py
@@ -11,81 +11,62 @@ from fim.utils.plots.fim_sde_quiver_plots import create_2D_quiver_plot
 
 from compute_mmd import get_mmd
 
+tau = 0.002 # [0.002, 0.01, 0.02]
+max_num_paths = 100
+
 class ModelResults:
-    def __init__(self, name: str, path: Path, **plot_kwargs):
-        self.name = name
+    def __init__(self, name: str, path: Path):
+        self.model_name = name
         self.path = path
         
         if str(path).endswith(".json"):
             with open(path, "r") as f:
                 data = json.load(f)
         self.data = data
-    
-
+        
+    def __call__(self, experiment_id: int, dataset_name: str):
+        if self.model_name == "GroundTruth":
+            dataset_idx = [d["name"] for d in self.data].index(dataset_name)
+            return torch.tensor(self.data[dataset_idx]["real_paths"][experiment_id])[:max_num_paths] # [num_paths, num_time_points, num_dims]
+        else:
+            dataset_idx = 0
+            for i, d in enumerate(self.data):
+                if d["name"] == dataset_name and d["tau"] == tau:
+                    dataset_idx = i
+                    break
+            return torch.tensor(self.data[dataset_idx]["synthetic_paths"][experiment_id])[:max_num_paths]
 
 @dataclass
 class Benchmark:
-    FIM_11M: ModelResults
-    FIM_20M: ModelResults
+    GroundTruth: ModelResults
+    FIM: ModelResults
     BISDE: ModelResults
     SparseGP: ModelResults
     
-
-def get_dataset_idx(data, dataset_name):
-    """
-    Fuck me that I have to write this function...
-    """
-    data_names = [d["name"] for d in data]
-    if 'Glycosis' in data_names:
-        data_names[data_names.index('Glycosis')] = "Glycolysis"
-    if 'Syn Drift' in data_names:
-        data_names[data_names.index('Syn Drift')] = "SynDrift"
-    if 'Double Well Max Diffusion' in data_names:
-        data_names[data_names.index('Double Well Max Diffusion')] = "DoubleWell"
-    if 'Damped Linear' in data_names:
-        data_names[data_names.index('Damped Linear')] = "DampedLinear"
-    if 'Damped Cubic' in data_names:
-        data_names[data_names.index('Damped Cubic')] = "DampedCubic"
-    if dataset_name not in data_names:
-        raise ValueError(f"Dataset {dataset_name} not found in {data_names}")
-    return data_names.index(dataset_name)
-
-
-def throw_away_nan_paths(estimated_paths, ground_truth_paths, dataset_name="", model_name=""):
-    original_num_paths = estimated_paths.shape[0]
-    # Detect which rows contain NaNs in estimated_paths
-    valid_idx = torch.tensor([i for i in range(original_num_paths) if not torch.isnan(estimated_paths[i]).any()], dtype=torch.long)
-    estimated_paths = estimated_paths[valid_idx]
-    ground_truth_paths = ground_truth_paths[valid_idx]
-    if len(valid_idx) < original_num_paths:
-        print(f"Warning: {len(estimated_paths) - len(valid_idx)} NaN paths in {dataset_name} for {model_name}")
-    return estimated_paths, ground_truth_paths    
+ground_truth_cache = {} # K_xx for the ground truth data is the same for all models, so we cache it 
 
 
 def run_benchmark(benchmark: Benchmark, evaluation_dir):
-    with open(evaluation_dir / "results.txt","w") as f: 
-        for dataset_name in ['Duffing', 'Glycolysis', 'SynDrift', 'Wang', 'DoubleWell', 'DampedLinear', 'DampedCubic']:
-            table = []
-            for model_name in ['FIM_11M', 'FIM_20M', 'BISDE', 'SparseGP']:
-                model = getattr(benchmark, model_name)
-                dataset_idx = get_dataset_idx(model.data, dataset_name)
-                data = model.data[dataset_idx]
-                # drift_rmse = rmse(data["estimated_drift_at_locations"], data["real_drift_at_locations"])
-                # diffusion_rmse = rmse(data["estimated_diffusion_at_locations"], data["real_diffusion_at_locations"])
-                ground_truth_paths = torch.tensor(data["real_paths"])
-                estimated_paths = torch.tensor(data["synthetic_paths"])
-                if not torch.isnan(estimated_paths).any():
-                    # mean_path_rmse = rmse(estimated_paths.mean(dim=0), ground_truth_paths.mean(dim=0))
-                    mmd = get_mmd(estimated_paths, ground_truth_paths)
-                    table.append([model_name, mmd])
-        
-            print(f"Results for {dataset_name}")
-            print(tabulate(table, headers=["Model", "MMD"]))
-            print("\n")
+    ground_truth = benchmark.GroundTruth
+    for experiment_id in range(5):
+        with open(evaluation_dir / f"results_{experiment_id}.txt","w") as f: 
+            for dataset_name in ['Damped Linear', 'Damped Cubic', 'Duffing', 'Glycosis', 'Hopf', 'Double Well', 'Wang', 'Syn Drift']:
+                table = []
+                for model_name in ['FIM', 'BISDE', 'SparseGP']:
+                    model = getattr(benchmark, model_name)
+                    ground_truth_paths = ground_truth(experiment_id, dataset_name)
+                    model_paths = model(experiment_id, dataset_name)
+                    if not torch.isnan(model_paths).any():
+                        mmd = get_mmd(ground_truth_paths, model_paths, kernel_cache=ground_truth_cache)
+                        table.append([model_name, mmd])
             
-            f.write(f"Results for {dataset_name}\n")
-            f.write(tabulate(table, headers=["Model", "MMD"]))
-            f.write("\n\n")
+                print(f"Results for {dataset_name}")
+                print(tabulate(table, headers=["Model", "MMD"]))
+                print("\n")
+                
+                f.write(f"Results for {dataset_name}\n")
+                f.write(tabulate(table, headers=["Model", "MMD"]))
+                f.write("\n\n")
     
 def rmse(m_pred, y_true):
    return (m_pred - y_true).pow(2).mean().sqrt()
@@ -97,15 +78,15 @@ if __name__ == "__main__":
     # Save dir setup: project_path / evaluations / synthetic_datasets / time_stamp + _ + experiment_descr
     evaluation_path =  "evaluations"
     time: str = str(datetime.now().strftime("%m%d%H%M"))
-    evaluation_dir: Path = Path(f"evaluations/ksig/{time}")
+    evaluation_dir: Path = Path(f"evaluations/ksig/{time}/{tau}")
     evaluation_dir.mkdir(parents=True, exist_ok=True)
     
     benchmarks = [
         Benchmark(
-            FIM_11M = ModelResults("FIM 11M", Path("data/processed/test/paths/fim_11M_params_stride_1_observations_many_paths.json")),
-            FIM_20M = ModelResults("FIM 20M", Path("data/processed/test/paths/fim_20M_params_trained_even_longer_stride_10_many_paths.json")),
-            BISDE = ModelResults("BISDE", Path("data/processed/test/paths/bisde_dense_observations_paths.json")),
-            SparseGP = ModelResults("SparseGP", Path("data/processed/test/paths/sparse_gp_dense_observations_many_paths.json")),
+            GroundTruth = ModelResults("GroundTruth", Path("/cephfs_projects/foundation_models/data/SDE/evaluation/20250129_coarse_synthetic_systems_5000_points_data/ksig_reference_paths.json")),
+            FIM = ModelResults("FIM", Path("/cephfs_projects/foundation_models/data/SDE/evaluation/20250129_coarse_synthetic_systems_5000_points_data/20M_trained_even_longer_synthetic_paths.json")),
+            #BISDE = ModelResults("BISDE", Path("data/processed/test/paths/bisde_dense_observations_paths.json")),
+            #SparseGP = ModelResults("SparseGP", Path("data/processed/test/paths/sparse_gp_dense_observations_many_paths.json")),
         ),
     ]
     
