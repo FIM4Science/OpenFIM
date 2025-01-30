60777fe0d35b16cd2ab1115f7ca62bf7662c65f3
David Berghaus <machs3ll@gmail.com>
2025-01-30 18:25:15 +0100

    Updated code

--- scripts/fim-sde/evaluation/parse_small_tables_to_big_table_mse.py
+++ scripts/fim-sde/evaluation/parse_small_tables_to_big_table_mse.py
@@ -37,7 +37,7 @@ def parse_results(file_path):
 
     return nested_dict
 
-def load_folder_path_results(folder_path, num_digits=3, scaling_fact=10):
+def load_folder_path_results(folder_path, num_digits=3):
     # Find all files in folder
     file_paths = [f"{folder_path}/{file}" for file in os.listdir(folder_path) if file.endswith('.txt')]
     # Check if any files are empty, if yes remove them
@@ -53,11 +53,7 @@ def load_folder_path_results(folder_path, num_digits=3, scaling_fact=10):
             for result in results:
                 if model in result[dataset]:
                     mmds.append(result[dataset][model])
-            mmds = np.array(mmds)
-            mmds = mmds*scaling_fact
             mean = np.mean(mmds)
-            if mean < 0:
-                mean = 0.0
             std = np.std(mmds)
             
             # Round to three decimal places
@@ -71,24 +67,18 @@ def load_folder_path_results(folder_path, num_digits=3, scaling_fact=10):
 
 # Example usage
 if __name__ == "__main__":
-    folder_paths = [('evaluations/ksig/01301144/0.002','evaluations/ksig/01301522/0.002'), ('evaluations/ksig/01301145/0.01','evaluations/ksig/01301522/0.01'), ('evaluations/ksig/01301146/0.02','evaluations/ksig/01301521/0.02')]
-    # folder_paths = ['evaluations/ksig/01301144/0.002', 'evaluations/ksig/01301145/0.01', 'evaluations/ksig/01301146/0.02']
+    folder_paths = ['evaluations/mse/01301754/0.002', 'evaluations/mse/01301756/0.01', 'evaluations/mse/01301756/0.02']
     datasets = ['Double Well', 'Wang', 'Damped Linear', 'Damped Cubic', 'Duffing', 'Glycosis', 'Hopf', 'Syn Drift']
     models = ["FIM", "BISDE", "SparseGP"]
     
-    with open("evaluations/final_table.txt", "w") as f:
+    with open("evaluations/final_table_mse.txt", "w") as f:
         f.write("\\begin{tabular}{llllllllll}\n")
         f.write("$\\tau$ & Model & Double Well & Wang & Damped Linear & Damped Cubic & Duffing & Glycolysis & Hopf & Syn Drift\\\\\n")
         f.write("\hline\n")
         
-        for (folder_path1, folder_path2) in folder_paths:
-            tau = folder_path1.split('/')[-1]
-            mean_results = load_folder_path_results(folder_path1)
-            bisde_results = load_folder_path_results(folder_path2)
-            # Merge the results
-            for dataset in datasets:
-                if "BISDE" in bisde_results[dataset]:
-                    mean_results[dataset]["BISDE"] = bisde_results[dataset]["BISDE"]
+        for folder_path in folder_paths:
+            tau = folder_path.split('/')[-1]
+            mean_results = load_folder_path_results(folder_path)
             for model in models:
                 row = f"{tau} & {model} & "
                 for dataset in datasets:
