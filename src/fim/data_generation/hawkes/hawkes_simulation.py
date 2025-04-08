1198f6eca09cb5a55f20e7d4c13ed540500d653b
David Berghaus <machs3ll@gmail.com>
2025-04-08 13:57:19 +0200

    Made artificial norm factor a cfg parameter

--- src/fim/data_generation/hawkes/hawkes_simulation.py
+++ src/fim/data_generation/hawkes/hawkes_simulation.py
@@ -62,6 +62,65 @@ def run_hawkes_simulation(baselines, kernel_grids, kernel_evaluations, num_paths
     return event_times, event_types, intensity_times, intensities
 
 
+def run_hawkes_simulation_cross_excitations(
+    baselines, kernel_grids, kernel_evaluations, num_paths, n_events_per_path, track_intensity, seed=0
+):
+    """
+    Run a Hawkes simulation with the given kernels.
+
+    Args:
+    baselines: np.array of length num_marks
+        The time independent intensities.
+    kernel_grids: list of num_marks np.arrays of np.arrays of length num_marks
+        The time grids on which the kernels get evaluated.
+    kernel_evaluations: list of num_marks np.arrays of np.arrays of length num_marks
+        The kernel evaluations.
+    num_paths: int
+        The number of paths to simulate.
+    n_events_per_path: int
+        The number of events per path.
+    track_intensity: bool
+        Whether to track the intensity.
+
+    Returns:
+    event_times: np.array [num_paths, n_events_per_path]
+        The event times.
+    event_types: np.array [num_paths, n_events_per_path]
+        The event types.
+    """
+    hawkes = SimuHawkes(baseline=baselines, max_jumps=n_events_per_path, seed=seed, verbose=False)
+    hawkes.threshold_negative_intensity(allow=True)
+    num_marks = len(kernel_grids)
+    for i in range(num_marks):
+        for j in range(num_marks):
+            kernel = HawkesKernelTimeFunc(t_values=kernel_grids[i][j], y_values=kernel_evaluations[i][j])
+            hawkes.set_kernel(i, j, kernel)
+
+    event_times = np.zeros((num_paths, n_events_per_path))
+    event_types = np.zeros((num_paths, n_events_per_path))
+    intensities = []
+    intensity_times = []
+    try:
+        for i in range(num_paths):
+            hawkes.reset()
+            hawkes.simulate()
+            event_times[i], event_types[i] = tick_timestamps_to_single_timeseries(hawkes.timestamps)
+            if track_intensity:
+                tracked_intensity = torch.tensor(np.array(hawkes.tracked_intensity))
+                tracked_intensity_times = torch.tensor(np.array(hawkes.intensity_tracked_times))
+                tracked_intensity_times = tracked_intensity_times - event_times[i][0]
+                intensities.append(tracked_intensity)
+                intensity_times.append(tracked_intensity_times)
+    except Exception as e:
+        print(f"Simulation failed with error: {e}")
+        return None, None, None, None
+
+    # Make sure that the events always start at 0
+    event_times = event_times - event_times[:, 0][:, None]
+
+    return event_times, event_types, intensity_times, intensities
+
+
 def tick_timestamps_to_single_timeseries(tick_timestamps):
     """
     The tick library returns the timestamps for every event type.
