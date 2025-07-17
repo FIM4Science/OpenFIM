b4801eda44acfdf6ff500f79245767a90234ee8d
pseifner <patrick@seifner.com>
2025-07-17 19:36:29 +0200

    SDE: Extract finetuning / training functions from FIM / LatentSDE on lorenz scripts.
    - These functions had been designed only for lorenz.
    - Want to reuse them for real world training / finetuning.
    - Extract them into helper functions.

--- scripts/sde/finetune_fim_sde_on_lorenz.py
+++ scripts/sde/finetune_fim_sde_on_lorenz.py
@@ -4,21 +4,14 @@
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
 import logging
-import os
 import warnings
 from pathlib import Path
 
 import click
-import torch
-from train_latent_sde import sample_paths_from_trained_model
-from transformers import AutoConfig
+import finetune_helpers
+from train_latent_sde import sample_lorenz_paths_from_trained_model
 
-from fim.data.dataloaders import DataLoaderFactory
-from fim.models import FIMSDE
-from fim.models.blocks import AModel, ModelFactory
-from fim.trainers.trainer import TrainerFactory
-from fim.trainers.utils import cleanup, clear_gpu_cache, get_accel_type, setup, setup_environ_flags
-from fim.utils.helper import expand_params, load_yaml
+from fim.utils.helper import load_yaml
 from fim.utils.logging import RankLoggerAdapter, setup_logging
 
 
@@ -28,106 +21,32 @@ warnings.filterwarnings("ignore", module="matplotlib")
 logger = RankLoggerAdapter(logging.getLogger(__name__))
 
 
-def finetune_fim(
+def finetune_fim_on_lorenz(
+    model_path: str,
     test_data_setups: dict,
-    config: Path,
-    seed: int,
-    exp_name: str,
-    train_data_label: str,
-    detach_diffusion: bool,
-    likelihood: bool,
-    sampling_mse: bool,
-    sampling_nll: bool,
-    num_points: int,
-    samples_count: int,
-    samples_steps: int,
-    em_steps: int,
-    sample_paths: bool,
-    epochs: int,
-    save_every: int,
-    lr: float,
-    weight_decay: float,
     train_from_scratch: bool,
+    base_config: Path,
+    train_data_label: str,
+    exp_name: str,
+    sample_paths: bool,
+    **extra_configs,
 ) -> None:
     """
-    Training config is loaded from file.
     FIM is loaded from checkpoint.
+    Training config is loaded from file.
     Optionally overwrite some train configs / model attributes manually before training.
     Sample paths from the finetuned model.
     """
-    config = load_yaml(config)
-    config["experiment"]["seed"] = seed
-    config["experiment"]["name"] = exp_name
+    model = finetune_helpers.load_pretrained_model(model_path, train_from_scratch)
+    base_config = load_yaml(base_config)
 
-    optimizer = config["optimizers"][0]["optimizer_d"]
-    optimizer["lr"] = lr
-    optimizer["weight_decay"] = weight_decay
-    config["optimizers"] = ({"optimizer_d": optimizer},)
+    config, model = finetune_helpers.add_extra_configs(base_config, model, exp_name, **extra_configs)
 
-    config["trainer"]["epochs"] = epochs
-    config["trainer"]["save_every"] = save_every
-
-    model_path = "/cephfs_projects/foundation_models/models/FIMSDE/NeurIPS_submission_models/600k_drift_deg_3_diff_deg_2_delta_tau_fixed_linear_attn_softmax_no_extra_normalization_and_fix_in_residual_layer_05-06-2300/checkpoints/epoch-139"
-
-    if train_from_scratch is False:
-        model: FIMSDE = AModel.load_model(model_path)
-
-    else:
-        model_config = AutoConfig.from_pretrained(Path(model_path) / "config.json")
-        model = ModelFactory.create(model_config)
-
-    model.finetune = True
-    model.config.finetune = True
-
-    model.finetune_samples_count = samples_count
-    model.config.finetune_samples_count = samples_count
-
-    model.finetune_samples_steps = samples_steps
-    model.config.finetune_samples_steps = samples_steps
-
-    model.finetune_em_steps = em_steps
-    model.config.finetune_em_steps = em_steps
-
-    model.finetune_detach_diffusion = detach_diffusion
-    model.config.finetune_detach_diffusion = detach_diffusion
-
-    model.finetune_on_likelihood = likelihood
-    model.config.finetune_on_likelihood = likelihood
-
-    model.finetune_on_sampling_mse = sampling_mse
-    model.config.finetune_on_sampling_mse = sampling_mse
-
-    model.finetune_on_sampling_nll = sampling_nll
-    model.config.finetune_on_sampling_nll = sampling_nll
-
-    model.finetune_num_points = num_points
-    model.config.finetune_num_points = num_points
-
-    gs_config = expand_params(config)[0]
-    gs_config.model = model.config
-
-    world_size = int(os.environ["WORLD_SIZE"])
-    rank = int(os.environ["RANK"])
-    local_rank = int(os.environ["LOCAL_RANK"])
-    setup(rank, world_size)
-
-    # saved parameters are sharded, so need to use torchrun for now
-    if torch.distributed.is_initialized():
-        torch.cuda.set_device(local_rank)
-        clear_gpu_cache(local_rank)
-        setup_environ_flags(rank)
-        device_map = gs_config.experiment.device_map
-        if device_map == "auto":
-            device_map = get_accel_type()
-
-        dataloader = DataLoaderFactory.create(**gs_config.dataset.to_dict())
-        trainer = TrainerFactory.create(gs_config.trainer.name, model=model, dataloader=dataloader, config=gs_config)
-        trainer.train()
-
-    cleanup()
+    trainer = finetune_helpers.train_fimsde(model, config)
+    finetune_helpers.add_model_type_to_checkpoints(trainer)
 
     if sample_paths is True:
-        sample_paths_from_trained_model(trainer, exp_name, train_data_label, test_data_setups)
+        sample_lorenz_paths_from_trained_model(trainer, exp_name, train_data_label, test_data_setups)
 
 
 if __name__ == "__main__":
@@ -171,8 +90,10 @@ if __name__ == "__main__":
         },
     }
 
+    model_path = "/cephfs_projects/foundation_models/models/FIMSDE/NeurIPS_submission_models/600k_drift_deg_3_diff_deg_2_delta_tau_fixed_linear_attn_softmax_no_extra_normalization_and_fix_in_residual_layer_05-06-2300/checkpoints/epoch-139"
+
     @click.command()
-    @click.option("--config", "config", type=click.Path(exists=True), required=True)
+    @click.option("--base-config", "base_config", type=click.Path(exists=True), required=True)
     @click.option("--seed", "seed", type=int, required=True)
     @click.option("--exp-name", "exp_name", type=str, required=True)
     @click.option("--train-data-label", "train_data_label", type=str, required=True)
@@ -191,7 +112,7 @@ if __name__ == "__main__":
     @click.option("--weight-decay", "weight_decay", type=float, required=False, default=0.0)
     @click.option("--train-from-scratch", "train_from_scratch", type=bool, required=False, default=False)
     def cli(**kwargs):
-        finetune_fim(test_data_setups, **kwargs)
+        finetune_fim_on_lorenz(model_path, test_data_setups, **kwargs)
 
     # pylint: disable=no-value-for-parameter
     cli()
---
+++ scripts/sde/finetune_helpers.py
@@ -0,0 +1,157 @@
+import json
+import os
+import re
+from pathlib import Path
+
+import torch
+from transformers import AutoConfig
+
+from fim import project_path
+from fim.data.dataloaders import DataLoaderFactory
+from fim.models import FIMSDE
+from fim.models.blocks import AModel, ModelFactory
+from fim.trainers.trainer import Trainer, TrainerFactory
+from fim.trainers.utils import cleanup, clear_gpu_cache, get_accel_type, setup, setup_environ_flags
+from fim.utils.helper import expand_params
+
+
+def load_pretrained_model(model_path: str, train_from_scratch: bool) -> FIMSDE:
+    """
+    From a checkpoint path, load the model and pretrained weights, or a new instance of model.
+
+    Args:
+        model_path (str): Absolute path to `.../checkpoints/epoch-...`.
+        train_from_scratch (bool): Flag to load pretrained weights.
+
+    Returns:
+        model (FIMSDE): Loaded instance of FIMSDE.
+    """
+    if train_from_scratch is False:
+        model: FIMSDE = AModel.load_model(model_path)
+
+    else:
+        model_config = AutoConfig.from_pretrained(Path(model_path) / "config.json")
+        model = ModelFactory.create(model_config)
+
+    return model
+
+
+def add_extra_configs(
+    config: dict,
+    model: FIMSDE,
+    exp_name: str,
+    seed: int,
+    detach_diffusion: bool,
+    likelihood: bool,
+    sampling_mse: bool,
+    sampling_nll: bool,
+    num_points: int,
+    samples_count: int,
+    samples_steps: int,
+    em_steps: int,
+    epochs: int,
+    save_every: int,
+    lr: float,
+    weight_decay: float,
+):
+    """
+    Adapt or add configs to a config dict (for trainer and dataloader) and model.
+    """
+    config["experiment"]["seed"] = seed
+    config["experiment"]["name"] = exp_name
+
+    optimizer = config["optimizers"][0]["optimizer_d"]
+    optimizer["lr"] = lr
+    optimizer["weight_decay"] = weight_decay
+    config["optimizers"] = ({"optimizer_d": optimizer},)
+
+    config["trainer"]["epochs"] = epochs
+    config["trainer"]["save_every"] = save_every
+
+    model.finetune = True
+    model.config.finetune = True
+
+    model.finetune_samples_count = samples_count
+    model.config.finetune_samples_count = samples_count
+
+    model.finetune_samples_steps = samples_steps
+    model.config.finetune_samples_steps = samples_steps
+
+    model.finetune_em_steps = em_steps
+    model.config.finetune_em_steps = em_steps
+
+    model.finetune_detach_diffusion = detach_diffusion
+    model.config.finetune_detach_diffusion = detach_diffusion
+
+    model.finetune_on_likelihood = likelihood
+    model.config.finetune_on_likelihood = likelihood
+
+    model.finetune_on_sampling_mse = sampling_mse
+    model.config.finetune_on_sampling_mse = sampling_mse
+
+    model.finetune_on_sampling_nll = sampling_nll
+    model.config.finetune_on_sampling_nll = sampling_nll
+
+    model.finetune_num_points = num_points
+    model.config.finetune_num_points = num_points
+
+    return config, model
+
+
+def train_fimsde(model: FIMSDE, config: dict):
+    """
+    Train a FIMSDE model using trainer and dataloader configured by config.
+    """
+    gs_config = expand_params(config)[0]
+    gs_config.model = model.config
+
+    world_size = int(os.environ["WORLD_SIZE"])
+    rank = int(os.environ["RANK"])
+    local_rank = int(os.environ["LOCAL_RANK"])
+    setup(rank, world_size)
+
+    # saved parameters are sharded, so need to use torchrun for now
+    if torch.distributed.is_initialized():
+        torch.cuda.set_device(local_rank)
+        clear_gpu_cache(local_rank)
+        setup_environ_flags(rank)
+        device_map = gs_config.experiment.device_map
+        if device_map == "auto":
+            device_map = get_accel_type()
+
+        dataloader = DataLoaderFactory.create(**gs_config.dataset.to_dict())
+        trainer = TrainerFactory.create(gs_config.trainer.name, model=model, dataloader=dataloader, config=gs_config)
+        trainer.train()
+
+    cleanup()
+
+    return trainer
+
+
+def add_model_type_to_checkpoints(trainer: Trainer) -> None:
+    """
+    Trainer does not add 'model_type' to config before saving.
+    Loading checkpoints requires 'model_type'. Thus, add it to configs in all checkpoints.
+    """
+    checkpoint_dir = Path(project_path) / trainer.checkpointer.checkpoint_dir
+
+    for checkpoint_name in [item.name for item in checkpoint_dir.iterdir() if item.is_dir()]:
+        config_path = checkpoint_dir / checkpoint_name / "config.json"
+        config = json.load(open(config_path, "r"))
+        config["model_type"] = trainer.model.config.model_type
+        json.dump(config, open(config_path, "w"))
+
+
+def get_last_epoch(checkpoint_dir: Path):
+    """
+    Return latest checkpoint in a list of checkpoints (which are named `epoch-X`).
+    """
+    epoch_numbers = []
+    for checkpoint_name in [item.name for item in checkpoint_dir.iterdir() if item.is_dir()]:
+        match = re.match(r"^epoch-(\d+)$", checkpoint_name)
+        if match:
+            epoch_numbers.append(int(match.group(1)))
+
+    last_epoch = f"epoch-{max(epoch_numbers)}"
+
+    return last_epoch
